ea3fd28e7c3469935d09bf3f6705b274f96b2e1d
aoba toshiki <aoba_toshiki@uniquevision.co.jp>
2025-06-25 15:30:48 +0900

    fix: make T: PartialOrd + Display implies RangeValidatable

--- garde/src/rules/range.rs
+++ garde/src/rules/range.rs
@@ -17,6 +17,17 @@ use std::ops::RangeBounds;
 
 use crate::error::Error;
 
+// Main apply function that works with the trait
+#[inline]
+pub fn apply<V, R, T>(v: &V, range: &R) -> Result<(), Error>
+where
+    V: RangeValidatable<T, R>,
+    R: RangeBounds<T>,
+    T: PartialOrd + Display,
+{
+    v.validate_range(range)
+}
+
 #[inline]
 fn apply_impl<T, R>(v: &T, range: &R) -> Result<(), Error>
 where
@@ -57,114 +68,20 @@ where
 }
 
 // Trait to extract the inner type for range validation
-pub trait RangeValidatable {
-    type Inner: PartialOrd + Display;
-    fn validate_range<R: RangeBounds<Self::Inner>>(&self, range: &R) -> Result<(), Error>;
+pub trait RangeValidatable<T: PartialOrd + Display, R: RangeBounds<T>> {
+    fn validate_range(&self, range: &R) -> Result<(), Error>;
 }
 
-// Use macros to implement for specific types to avoid conflicts
-macro_rules! impl_range_validatable {
-    ($($T:ty),*) => {
-        $(
-            impl RangeValidatable for $T {
-                type Inner = $T;
-
-                #[inline]
-                fn validate_range<R: RangeBounds<Self::Inner>>(&self, range: &R) -> Result<(), Error> {
-                    apply_impl(self, range)
-                }
-            }
-
-            impl RangeValidatable for Option<$T> {
-                type Inner = $T;
-
-                #[inline]
-                fn validate_range<R: RangeBounds<Self::Inner>>(&self, range: &R) -> Result<(), Error> {
-                    match self {
-                        Some(val) => apply_impl(val, range),
-                        None => Ok(()),
-                    }
-                }
-            }
-        )*
-    };
+impl<T: PartialOrd + Display, R: RangeBounds<T>> RangeValidatable<T, R> for T {
+    fn validate_range(&self, range: &R) -> Result<(), Error> {
+        apply_impl(self, range)
+    }
 }
 
-impl_range_validatable!(u8, u16, u32, u64, usize, u128, i8, i16, i32, i64, isize, i128, f32, f64);
-
-// Main apply function that works with the trait
-#[inline]
-pub fn apply<V, R>(v: &V, range: &R) -> Result<(), Error>
-where
-    V: RangeValidatable,
-    R: RangeBounds<V::Inner>,
-{
-    v.validate_range(range)
-}
-
-pub trait Bounds: PartialOrd {
-    type Size: Copy + Sized + Display;
-
-    const MIN: Self::Size;
-    const MAX: Self::Size;
-
-    fn validate_bounds(
-        &self,
-        lower_bound: Self::Size,
-        upper_bound: Self::Size,
-    ) -> Result<(), OutOfBounds>;
-}
-
-pub enum OutOfBounds {
-    Lower,
-    Upper,
-}
-
-macro_rules! impl_for {
-    ($($T:ty),*) => {
-        $(
-            impl Bounds for $T {
-                type Size = $T;
-
-                const MIN: Self::Size = <$T>::MIN;
-                const MAX: Self::Size = <$T>::MAX;
-
-                fn validate_bounds(
-                    &self,
-                    lower_bound: Self::Size,
-                    upper_bound: Self::Size,
-                ) -> Result<(), OutOfBounds> {
-                    if self < &lower_bound {
-                        Err(OutOfBounds::Lower)
-                    } else if self > &upper_bound {
-                        Err(OutOfBounds::Upper)
-                    } else {
-                        Ok(())
-                    }
-                }
-            }
-        )*
-    };
-}
-
-impl_for!(u8, u16, u32, u64, usize, u128, i8, i16, i32, i64, isize, i128, f32, f64);
-
-#[cfg(feature = "rust_decimal")]
-impl_for!(rust_decimal::Decimal);
-
-impl<T: Bounds> Bounds for Option<T> {
-    type Size = T::Size;
-
-    const MIN: Self::Size = T::MIN;
-    const MAX: Self::Size = T::MAX;
-
-    fn validate_bounds(
-        &self,
-        lower_bound: Self::Size,
-        upper_bound: Self::Size,
-    ) -> Result<(), OutOfBounds> {
+impl<T: PartialOrd + Display, U: RangeBounds<T>> RangeValidatable<T, U> for Option<T> {
+    fn validate_range(&self, range: &U) -> Result<(), Error> {
         match self {
-            Some(value) => value.validate_bounds(lower_bound, upper_bound),
+            Some(val) => apply_impl(val, range),
             None => Ok(()),
         }
     }
